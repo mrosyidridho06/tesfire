4744304b5ec87e96fcb5a6a733e52e9a456ef12e
mrosyidridho06 <61150937+mrosyidridho06@users.noreply.github.com>
2021-02-25 10:48:58 +0800

    update chart di arus

--- public/aru1.js
+++ public/aru1.js
@@ -1,206 +1,74 @@
 function Moin(){
-    // $(document).ready(function () {
-    //     // initialize Global variables
-    //     var dataArr = [];
-    //     var previousVal = '';
-    //     var compareVal ='';
-    //     var chartCount ;
-    //     var currentFnD='';
-    //     var ref = firebase.database().ref('PowerMetering/Home-Monitoring');
-    //     function getData() {
-            
-    //         ref.on("child_added", function (snapshot) {
-    //             var obj = snapshot.child('Irms').val();
-    //             currentFnd=obj;
-    //             if (currentFnd === previousVal && typeof obj !=='undefined') {
-    //                 chartCount++;
-    //                 //currentFnD=obj.foodAndDrinks;
-    //             }
-    //             else if(typeof obj ==='undefined' || obj === ''){
-    //                 console.log(chartCount, obj.label);
-    //                 chartCount=0;
-    //             }
-    //             else if (currentFnd !== previousVal && typeof obj !=='undefined'  || obj !== ' ' || obj !== "null") {
-    //                 chartCount++;
-    //                 dataArr = ({
-    //                     label: previousVal,
-    //                     value: chartCount,
-    //                 });
-                
-    //                 chartCount = 0;
-    //                 previousVal = obj;
-    //                 //currentFnd=obj.foodAndDrinks;
-    //             }
-            
-    //           // Verify that values are defined and push data into the chart    
-    //             if(typeof dataArr.label !== 'undefined' && typeof dataArr.value !=='undefined'){
-    //                if(dataArr.label !== compareVal){
-    //                     addData(myChart, dataArr.label, dataArr.value);
-    //                     compareVal = dataArr.label;
-    //                }     
-                   
-                    
-    //             }
-                
-    //             // var unique = $.makeArray($(dataArr).filter(function(i,itm){
-    //             //     return i == $(dataArr).index(itm);
-    //             // })); 
-    
-    //         });
-    
-    //     }
-    
-    //     window.chartColors = {
-    //         red: 'rgb(255, 99, 132)',
-    //         orange: 'rgb(255, 159, 64)',
-    //         yellow: 'rgb(255, 205, 86)',
-    //         green: 'rgb(75, 192, 192)',
-    //         blue: 'rgb(54, 162, 235)',
-    //         purple: 'rgb(153, 102, 255)',
-    //         grey: 'rgb(201, 203, 207)'
-    //     };
-    //   // Initialize the Chart Element on HTML 
-    //     var ctx = document.getElementById("myChart").getContext("2d");
-    //     options = {
-    //         scales: {
-    //             xAxes: [{
-    //                 barPercentage: 0.5,
-    //                 barThickness: 6,
-    //                 maxBarThickness: 8,
-    //                 minBarLength: 2,
-    //                 gridLines: {
-    //                     offsetGridLines: true
-    //                 },
-    //                 yAxes: [{
-    //                     ticks: {
-    //                         min: 0,
-    //                         max: 100,
-    //                         stepSize: 10
-    //                     }
-    //                 }]
-    //             }],
-    //             layout: {
-    //                 padding: {
-    //                     left: 50,
-    //                     right: 0,
-    //                     top: 0,
-    //                     bottom: 0
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     // Set the configuration of the Chart elements.
-    //     // This is where we pass the data we captured from firebase to the chart label and data
-    //     var config = {
-    //         type: 'line',
-    //         data: {
-    //             labels: dataArr.label,       // value of label retrieved from firebase
-    //             datasets: [{
-    //                 label: "User Views by Cuisine",
-    //                 backgroundColor: "rgb(255, 159, 64)",
-    //                 borderColor: "rgba(54, 162, 235, 1)",
-    //                 borderWidth: 1,
-    //                 data: dataArr.value     // value of search for each of the array elements
-    //             }],
-    //             responsive: true,
-    //             options: options
-    
-    //         }
-    //     };
-    
-    //     // Create new chart element context with refrence to the HTML element
-    //     var myChart = new Chart(ctx, config);
-    
-       
-    //     // Put data into the dataset for the chart and update the chart
-    //     function addData(chart, label, data) {
-    //         chart.data.labels.push(label);
-    //         chart.data.datasets.forEach((dataset) => {
-    //             dataset.data.push(data);
-    //         });
-    //         chart.update();
-    //     }
-    //     // Call the function which initiates the chart setup
-    //     getData();
-    // })
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//         labels: 'Arus',
-//         datasets: [
-//           {
-//             data: [0],
-//           }
-//         ]
-//       },
-//       options: {
-//         scales: {
-//           xAxes: [ {
-//               display: true,
-//               type: 'time',
-//               time: {
-//                 parser: 'MM/DD/YYYY HH:mm',
-//                 tooltipFormat: 'll HH:mm',
-//                 unit: 'day',
-//                 unitStepSize: 1,
-//                 displayFormats: {
-//                   'day': 'MM/DD/YYYY'
-//                 }
-//               }
-//             }
-//           ],
-//           yAxis: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             // etc...
-//           }],
-//         }
-//       }
-//     }
-//   );
         type: 'line',
+        value:'Ampere',
         data: {
-            labels: ['Value'],
-            dataset: [{
-                labels:'Arus',
-                data:[]
+            labels: [],
+            datasets: [{
+                label:'Arus',
+                data: [{}],
+                backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)'
+                            ],
+                            borderWidth: 1
             }]
-            // value:'Arus',
-            // data: {
-            //     datasets: [{
-            //         labels: 'Arus',
-            //         data:[{}],
-            //         backgroundColor: [
-            //             'rgba(255, 99, 132, 0.2)'
-            //         ],
-            //         borderColor: [
-            //             'rgba(255, 99, 132, 1)'
-            //         ],
-            //         borderWidth: 1
-                
-            //     }]
-            // },
-            
         },
-        options: {
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    distribution: 'series',
-                    time:{
-                        unit: 'second'
+            options: {
+                responsive:true,
+                title: {
+                    display: true,
+                    text: 'Chart Arus'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: true
+                },
+                annotation: {
+                    annotations: [{
+                    type: 'line',
+                    mode: 'horizontal',
+                    scaleID: 'y-axis-0',
+                    value: 5,
+                    borderColor: 'rgb(75, 192, 192)',
+                    borderWidth: 4,
+                    label: {
+                        enabled: false,
+                        content: 'Test label'
                     }
-                }],
-                yAxes: [{
-                    ticks: {
-                            beginAtZero: true
+                    }]
+                },
+                scales: {
+                    display:true,
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'month'
+                        }
                     }
-                }]
-            }
-        },
-    })
+                }
+                // scales:{
+                //     yAxes:[{
+                //         type:'time',
+                //         time:{
+                //             unit: 'day',
+                //             unitStepSize: 1,
+                //             displayFormats:{
+                //                 'day': 'MMM DD'
+                //             }
+                //         }
+                //     }]
+                // },
+                // callback: function(tickValue, index, ticks) {
+                //     if (!(index % parseInt(ticks.length / 5))) {
+                //       return moment.duration(tickValue/100, 's').format('mm:ss.SS');
+                //     }
+                // }
+        }
+    });
 
     function addData(chart, label, data) {
         chart.data.labels.push(label);
@@ -210,30 +78,23 @@ function Moin(){
         chart.update();
         console.log(data)
     }
-
-    function removeData(chart) {
-        chart.data.labels.pop();
-        chart.data.datasets.forEach((dataset) => {
-            dataset.data.pop();
-        });
-        chart.update();
-        
-    }
+    // function updateScale(chart) {
+    //     chart.options.scales.yAxes[{}] = {
+    //         type: 'line',
+    //         data:[]
+    //     };
+    
+    // }  
     var valueRef = firebase.database().ref('PowerMetering/Home-Monitoring').child('Irms');
     valueRef.on('value', function(snapshot) {
-      removeData(myChart);
-      addData(myChart,'Value',snapshot.val());
-      console.log(addData)
+        // removeData(myChart);
+        // updateScale(myChart);
+        addData(myChart,'value',snapshot.val());
+        console.log(myChart)
     });
-    
-}
 
-
-window.onload = function(){
-    Moin()
 }
-// callback: function(tickValue, index, ticks) {
-//     if (!(index % parseInt(ticks.length / 5))) {
-//       return moment.duration(tickValue/100, 's').format('mm:ss.SS');
-//     }
-// }
+    window.onload = function(){
+        Moin();
+    }
+
--- public/arus.js
+++ public/arus.js
@@ -299,3 +299,135 @@ window.onload = function(){
 //                   }
 //                 ]
 //             }
+ // $(document).ready(function () {
+    //     // initialize Global variables
+    //     var dataArr = [];
+    //     var previousVal = '';
+    //     var compareVal ='';
+    //     var chartCount ;
+    //     var currentFnD='';
+    //     var ref = firebase.database().ref('PowerMetering/Home-Monitoring');
+    //     function getData() {
+            
+    //         ref.on("child_added", function (snapshot) {
+    //             var obj = snapshot.child('Irms').val();
+    //             currentFnd=obj;
+    //             if (currentFnd === previousVal && typeof obj !=='undefined') {
+    //                 chartCount++;
+    //                 //currentFnD=obj.foodAndDrinks;
+    //             }
+    //             else if(typeof obj ==='undefined' || obj === ''){
+    //                 console.log(chartCount, obj.label);
+    //                 chartCount=0;
+    //             }
+    //             else if (currentFnd !== previousVal && typeof obj !=='undefined'  || obj !== ' ' || obj !== "null") {
+    //                 chartCount++;
+    //                 dataArr = ({
+    //                     label: previousVal,
+    //                     value: chartCount,
+    //                 });
+                
+    //                 chartCount = 0;
+    //                 previousVal = obj;
+    //                 //currentFnd=obj.foodAndDrinks;
+    //             }
+            
+    //           // Verify that values are defined and push data into the chart    
+    //             if(typeof dataArr.label !== 'undefined' && typeof dataArr.value !=='undefined'){
+    //                if(dataArr.label !== compareVal){
+    //                     addData(myChart, dataArr.label, dataArr.value);
+    //                     compareVal = dataArr.label;
+    //                }     
+                   
+                    
+    //             }
+                
+    //             // var unique = $.makeArray($(dataArr).filter(function(i,itm){
+    //             //     return i == $(dataArr).index(itm);
+    //             // })); 
+    
+    //         });
+    
+    //     }
+    
+    //     window.chartColors = {
+    //         red: 'rgb(255, 99, 132)',
+    //         orange: 'rgb(255, 159, 64)',
+    //         yellow: 'rgb(255, 205, 86)',
+    //         green: 'rgb(75, 192, 192)',
+    //         blue: 'rgb(54, 162, 235)',
+    //         purple: 'rgb(153, 102, 255)',
+    //         grey: 'rgb(201, 203, 207)'
+    //     };
+    //   // Initialize the Chart Element on HTML 
+        // var ctx = document.getElementById("myChart").getContext("2d");
+        // options = {
+    //         scales: {
+    //             xAxes: [{
+    //                 barPercentage: 0.5,
+    //                 barThickness: 6,
+    //                 maxBarThickness: 8,
+    //                 minBarLength: 2,
+    //                 gridLines: {
+    //                     offsetGridLines: true
+    //                 },
+    //                 yAxes: [{
+    //                     ticks: {
+    //                         min: 0,
+    //                         max: 100,
+    //                         stepSize: 10
+    //                     }
+    //                 }]
+    //             }],
+    //             layout: {
+    //                 padding: {
+    //                     left: 50,
+    //                     right: 0,
+    //                     top: 0,
+    //                     bottom: 0
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     // Set the configuration of the Chart elements.
+    //     // This is where we pass the data we captured from firebase to the chart label and data
+    //     var config = {
+    //         type: 'line',
+    //         data: {
+    //             labels: dataArr.label,       // value of label retrieved from firebase
+    //             datasets: [{
+    //                 label: "User Views by Cuisine",
+    //                 backgroundColor: "rgb(255, 159, 64)",
+    //                 borderColor: "rgba(54, 162, 235, 1)",
+    //                 borderWidth: 1,
+    //                 data: dataArr.value     // value of search for each of the array elements
+    //             }],
+    //             responsive: true,
+    //             options: options
+    
+    //         }
+    //     };
+    
+    //     // Create new chart element context with refrence to the HTML element
+    //     var myChart = new Chart(ctx, config);
+    
+       
+    //     // Put data into the dataset for the chart and update the chart
+    //     function addData(chart, label, data) {
+    //         chart.data.labels.push(label);
+    //         chart.data.datasets.forEach((dataset) => {
+    //             dataset.data.push(data);
+    //         });
+    //         chart.update();
+    //     }
+    //     // Call the function which initiates the chart setup
+    //     getData();
+    // })
+    // var moment = require('moment'); // require
+    // moment().format(); 
+    // Use moment.updateLocale(localeName, config)
+    // moment.defineLocale(localeName, config)
+    // const a = '08-01-2019';
+    // const outputDateFormat = 'MM-DD-YYYY';
+    // const b = moment(a, 'MM-DD-YYYY').add(1, 'years').add(-1, 'days').format(outputDateFormat);
+    // console.log(b);
